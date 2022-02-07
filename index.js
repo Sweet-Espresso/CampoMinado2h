c53322415e84409692973736412629964857d215
Sweet-Espresso <97476773+Sweet-Espresso@users.noreply.github.com>
2022-02-07 15:56:10 -0300

    Mudando a porta

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.listen(3000, () => {
-  console.log("ok")
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
